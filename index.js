7305fa07ae8a16075508bfccce62656f55628a56
hrit99 <hritikpihal51299@gmail.com>
2023-08-08 23:04:35 +0530

    deploy8

--- index.js
+++ index.js
@@ -5,27 +5,16 @@ const { default: mongoose } = require('mongoose')
 const uri = "mongodb+srv://hritik:DV3zW2dyz5sd8Dd4@cluster0.7xhuq.mongodb.net/moviefinder?retryWrites=true&w=majority";
 const bodyParser = require('body-parser');
 const path = require('path')
-const compression = require("compression");
-const helmet = require("helmet");
+
 
 const app = express()
 
-app.set('view engine', 'ejs');
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors())
-app.use(compression());
 app.use('/static', express.static(path.join(__dirname, 'public')))
 
-app.use(
-  helmet.contentSecurityPolicy({
-    directives: {
-      "script-src": ["'self'", "code.jquery.com", "cdn.jsdelivr.net"],
-    },
-  }),
-);
 
-//a comment
 
 mongoose.connect(uri, {useNewUrlParser:true, useUnifiedTopology:true})
 
