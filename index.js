2a6d625b0af9e94b95d099bc06cbd7395699b9b7
hrit99 <hritikpihal51299@gmail.com>
2023-08-08 18:06:10 +0530

    deploying

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const bodyParser = require('body-parser');
 
 const app = express()
 app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors())
 
 mongoose.connect(uri, {useNewUrlParser:true, useUnifiedTopology:true})
