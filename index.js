c263f579b006e3afa5fe59ffdc4c5ae54b7a9161
hrit99 <hritikpihal51299@gmail.com>
2023-08-09 12:22:57 +0530

    trying render

--- index.js
+++ index.js
@@ -1,46 +1,43 @@
 const express = require('express')
-// var cors = require('cors')
-// const router = require('./routes/router')
-// const { default: mongoose } = require('mongoose')
-// const uri = "mongodb+srv://hritik:DV3zW2dyz5sd8Dd4@cluster0.7xhuq.mongodb.net/moviefinder?retryWrites=true&w=majority";
+var cors = require('cors')
+const router = require('./routes/router')
+const { default: mongoose } = require('mongoose')
+const uri = "mongodb+srv://hritik:DV3zW2dyz5sd8Dd4@cluster0.7xhuq.mongodb.net/moviefinder?retryWrites=true&w=majority";
 const bodyParser = require('body-parser');
-// const path = require('path')
+const path = require('path')
 
 
 const app = express()
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(cors())
-// app.use('/static', express.static(path.join(__dirname, 'public')))
+app.use(cors())
+app.use('/static', express.static(path.join(__dirname, 'public')))
 
 
 
-// mongoose.connect(uri, {useNewUrlParser:true, useUnifiedTopology:true})
+mongoose.connect(uri, {useNewUrlParser:true, useUnifiedTopology:true})
 
-// const db = mongoose.connection
+const db = mongoose.connection
 
-// db.on('error', (err)=>{
-//   console.log(err);
-// })
+db.on('error', (err)=>{
+  console.log(err);
+})
 
-// db.once('open', () => {
-//   console.log('Database connected');
-// })
+db.once('open', () => {
+  console.log('Database connected');
+})
 
-// app.use((req,res,next)=>{
-//     res.setHeader('Access-Control-Allow-Origin', '*');
-//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
-//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-//     next();
-// })
+app.use((req,res,next)=>{
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+})
 
-// //routing
-// app.use(router)
-app.get("/", (req, res) => {
-    res.send("Express on Vercel");
-  });
-  
+//routing
+app.use(router)
+
 app.listen(3000)
 
 module.exports = app;
